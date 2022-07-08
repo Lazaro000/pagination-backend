c970a1720215ea2a83d87297db3afcc71416798d
Lázaro <lazaroyaesta@gmail.com>
2022-07-08 21:33:21 +0200

    ✨ Add User Resource

---
+++ src/main/java/io/lazaro/pagination/resource/UserResource.java
@@ -0,0 +1,44 @@
+package io.lazaro.pagination.resource;
+
+import io.lazaro.pagination.domain.HttpResponse;
+import io.lazaro.pagination.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+
+/**
+ * @author Lázaro
+ * @version 1.0
+ * @since 08/07/2022
+ */
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<HttpResponse> getUsers(@RequestParam Optional<String> name,
+                                                 @RequestParam Optional<Integer> page,
+                                                 @RequestParam Optional<Integer> size) {
+        return ResponseEntity.ok().body(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .data(Map.of("page", userService.getUsers(name.orElse(""), page.orElse(0), size.orElse(10))))
+                        .message("Users Retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+}
